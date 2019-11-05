02187b2244a957406bbf3aea456aaae364c1cd1d
Sebastian Diaz <seadiaz@gmail.com>
2019-11-05 19:52:18 -0300

    feat[cli]: add support for load memberships

---
+++ src/cli/memberships.go
@@ -0,0 +1,51 @@
+package cli
+
+// Membership ...
+type Membership struct {
+	PersonEmail string
+	TeamName    string
+	Team        *Team
+}
+
+// LoadMemberships ...
+func (c *Client) LoadMemberships() {
+	rawData := readCsvFile(c.Filename)
+	parsedData := mapArrayToMemberships(rawData)
+	teams := c.getTeams()
+	parsedData = fulfillMembershipTeamFromTeamName(parsedData, teams)
+	c.postMemberships(parsedData)
+}
+
+func mapArrayToMemberships(array [][]string) []*Membership {
+	output := make([]*Membership, 0, 0)
+	for _, item := range array {
+		output = append(output, &Membership{
+			TeamName:    item[0],
+			PersonEmail: item[1],
+		})
+	}
+	return output
+}
+
+func fulfillMembershipTeamFromTeamName(Memberships []*Membership, teams []*Team) []*Membership {
+	output := make([]*Membership, 0, 0)
+	for _, item := range Memberships {
+		team := findTeamByName(teams, item.TeamName)
+		if team != nil {
+			item.Team = team
+		}
+		output = append(output, item)
+	}
+	return output
+}
+
+func (c *Client) postMemberships(Memberships []*Membership) {
+	for _, item := range Memberships {
+		c.postMembership(item)
+	}
+}
+
+func (c *Client) postMembership(membership *Membership) {
+	body := &Person{Email: membership.PersonEmail}
+	doPostRequest(body, c.URL+teamsPath+"/"+membership.Team.ID+peoplePath)
+}
---
+++ src/cli/teams.go
@@ -0,0 +1,58 @@
+package cli
+
+const teamsPath = "/teams"
+
+// Team ...
+type Team struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name"`
+}
+
+// LoadTeams ...
+func (c *Client) LoadTeams() {
+	rawData := readCsvFile(c.Filename)
+	parsedData := mapArrayToTeams(rawData)
+	c.postTeams(parsedData)
+}
+
+func mapArrayToTeams(array [][]string) []*Team {
+	output := make([]*Team, 0, 0)
+	for _, item := range array {
+		output = append(output, &Team{
+			Name: item[0],
+		})
+	}
+	return output
+}
+
+func (c *Client) postTeams(teams []*Team) {
+	for _, item := range teams {
+		c.postTeam(item)
+	}
+}
+
+func (c *Client) postTeam(team *Team) {
+	doPostRequest(team, c.URL+teamsPath)
+}
+
+func findTeamByName(teams []*Team, name string) *Team {
+	for _, item := range teams {
+		if item.Name == name {
+			return item
+		}
+	}
+
+	return nil
+}
+
+func (c *Client) getTeams() []*Team {
+	res := doGetRequest(c.URL + teamsPath)
+	output := make([]*Team, 0, 0)
+	for _, item := range res {
+		output = append(output, &Team{
+			ID:   item["id"].(string),
+			Name: item["name"].(string),
+		})
+	}
+	return output
+}
